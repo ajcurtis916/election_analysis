1390860ec9f156818f5f7a8a9c7836581b85baf2
Alexandra Curtis <ajcurtis916@gmail.com>
2021-10-09 19:21:00 -0700

    updates

--- PyPoll.py
+++ PyPoll.py
@@ -5,20 +5,10 @@
 #4. total # of votes each candidate won
 #5. winner of election based on pop vote
 
-#import datetime class from datetime module
-# import datetime as dt
-# set variable and use now() attribute on datetime class to get present time
-# now=dt.datetime.now()
-#print present time
-# print("The time right now is",now)
 
 #importing the operating system module and defining the module's attributes
 import csv
 import os
-# print(dir(os))
-
-#submodule function discovery
-# print(dir(os.path))
 
 #open file using different code
 # with open('election_results.csv') as election_data:
@@ -35,61 +25,101 @@ import os
 
 #assign variable and path to csv
 # load_election_data=os.path.join("resources", "election_results.csv")
-load_election_data = "resources/election_results.csv"
+load_election_data=os.path.join("resources","election_results.csv")
+#create filename variable to a path to save the file (format previously had issues so used r string)
+file_to_save=os.path.join("analysis","election_results.txt")
+# "C:\Users\acurt\OneDrive\Desktop\Data Analytics Bootcamp\M3 Python\election_analysis\analysis\election_results.txt"
 
 # print(f"my input file: {load_election_data}")
 # exit()
 
+#Initialize total vote counter w/ variable
+total_votes=0
+
+#declare new list
+candidates=[]
+
+#declare dictionary to connect candidates w/ their votes
+candidate_votes={}
+
+#winning candidate and winning count tracker
+winning_candidate=""
+winning_count=0
+winning_percentage=0
+
 #open and read file
 with open(load_election_data) as election_data:
-    print(election_data)
-
     #read the file object with the reader function
     file_reader=csv.reader(election_data)
+    
     #print header row
     headers=next(file_reader)
-    print(headers)
+     
+    #print ea row in the CSV file
+    for rows in file_reader:
+        #add to the total vote count
+        total_votes+=1 
+        
+        #print the candidate name for ea row
+        candidate_name=rows[2]  
+
+            #if candidate does not match any existing candidate...
+        if candidate_name not in candidates:
+            #add to list of candidates
+            candidates.append(candidate_name)
+
+            #begin tracking ea candidate's vote count (initialize)
+            candidate_votes[candidate_name]=0
+
+        #add vote to that candidate's count
+        candidate_votes[candidate_name]+=1
+    
+#determine the % of votes for ea candidate by looping through the counts
+#1. Iterate through the candidate list
+        for candidate_name in candidate_votes:
+    #2. Retreive vote count of ea condidate
+            votes=candidate_votes[candidate_name]
+    #3. Calculate % of the votes
+            vote_percentage=float(votes)/float(total_votes)*100
     
     
-#for loop using break exercise w/ tutor
-# x = [1, 2, 3, 4]
+            candidate_results=(f"{candidate_name}:{vote_percentage:.1f}%({votes:,})\n")
+            print(candidate_results)
 
-# for num in x:
-#     if num == 2:
-#         print(f"if num= {num}")
-#         exit()
-#     elif num == 3: 
-#         print(f"I am 3.")   
-#         break
-#     else:
-#         print(f"else num= {num}")
+        #using the with statement open the file as text (makes code cleaner more readable)
+        with open(file_to_save,"w") as txt_file:
+        # #write some data to the file
+            txt_file.write(str(candidate_results)) #leads to infinite loop?
 
-# # exit()
+# with open(file_to_save,"w") as txt_file:
+    # txt_file.write(candidate_results)
+    # 4. Print the candidate name and % of votes
+    # print(f"{candidate_name} received {vote_percentage}% of the votes.")
+    # print(f"{vote_percentage:.1f}%")
 
-# print(f"my input file: 1")
-
-# print(f"my input file: 2")
-
-# exit()
-
-# print(f"my input file: 3")
+# #Print total votes,candidates, votes per candidate (checking work)
+# print(total_votes)
+# print(candidates) 
+# print(candidate_votes)
 
 
-#create filename variable to a path to save the file (format previously had issues so used r string)
-file_to_save=r"C:\Users\acurt\OneDrive\Desktop\Data Analytics Bootcamp\M3 Python\election_analysis\analysis\election_results.txt"
-#using the with statement open the file as a text file
-# outfile=open(file_to_save, "w")
-#write some data to the file
-# outfile.write("Hello World")
+    #Determine winning vote count and candidate IMPORTANTE
+    #1. Determine if the votes are greater than the winning count
+    if (votes>winning_count) and (vote_percentage>winning_percentage):
+    #2 If true then set winning_count=votes and winning_percent=
+    #vote+percentage
+        winning_count=votes
+        winning_percentage=vote_percentage
+    #3 Set the winning_candidate equal to the candidate's name
+        winning_candidate=candidate_name 
 
-#close the file
-# outfile.close()
-
-#using the with statement open the file as text (makes code cleaner more readable)
-with open(file_to_save,"w") as txt_file:
-    #write some data to the file
-    txt_file.write("Counties in the Election\n-------------------------\nArapahoe\nDenver\nJefferson")
-
+    winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+print(winning_candidate_summary)            
 
 
 
